61b3f5e84c629a16fcb8fbd8d96ef1db513082a9
Jus1927 <justinmarkh@gmail.com>
2024-04-27 19:50:57 -0700

    link script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//document.getElementById(root).innerHTML = new <p
-// this is where p much everything will be
+//DOMContentLoaded to prevent error on unloaded content
+document.addEventListener('DOMContentLoaded', function() {
+    // Create an "li" node:
+    const node = document.createElement("li");
+
+    // Create a text node:
+    const textnode = document.createTextNode("is it working?!");
+    const textnode2 = document.createTextNode("the second one!");
+
+    // Append the text node to the "li" node:
+    node.appendChild(textnode);
+    node.appendChild(textnode2)
+
+    // Append the "li" node to the list:
+    document.getElementById("root").appendChild(node);
+});
+
+
+
