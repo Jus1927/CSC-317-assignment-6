5be95166320cfb5a52d6c0a3273c262619ef3be9
Jus1927 <justinmarkh@gmail.com>
2024-04-27 19:56:27 -0700

    add text area

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 //DOMContentLoaded to prevent error on unloaded content
 document.addEventListener('DOMContentLoaded', function() {
-    // Create an "li" node:
-    const node = document.createElement("li");
-
-    // Create a text node:
-    const textnode = document.createTextNode("is it working?!");
-    const textnode2 = document.createTextNode("the second one!");
-
-    // Append the text node to the "li" node:
-    node.appendChild(textnode);
-    node.appendChild(textnode2)
+    
+    //create textarea
+    const textarea = document.createElement('textarea');
 
     // Append the "li" node to the list:
-    document.getElementById("root").appendChild(node);
+    document.getElementById("root").appendChild(textarea);
 });
 
 
